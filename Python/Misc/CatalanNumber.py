03ad134859fe8000ba36022d4ae46431bc995ff7
Vipul Singh <vipul21sept@gmail.com>
2019-07-25 21:56:35 -0700

    Catalan Number

---
+++ Python/Misc/CatalanNumber.py
@@ -0,0 +1,17 @@
+import collections
+def catalanNumber(n):
+    d = collections.defaultdict(int)
+    d[0] = 1
+
+    def calculate(val):
+        s = 0
+        print(val)
+        for j in range(val):
+            # print(s, d[j], d[val])
+            s += d[j]*d[val-j-1]
+        return s
+    for i in range(1, n+1):
+        d[i] = calculate(i)
+    return d[n]
+print(catalanNumber(5))
+# Cn+1 = SUM(0-n) over Ci*Ci-n
